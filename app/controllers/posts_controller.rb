5f7a7424c4b0af8c06206cfbeeed6670a7cd7d4a
mistake-git <acbmstk0402@gmail.com>
2019-11-24 00:33:21 +0000

    画像投稿機能を作成しました

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -70,6 +70,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:name, :feed,:weather,:description,:number,:date)
+      params.require(:post).permit(:image,:name, :feed,:weather,:description,:number,:date)
     end
 end
