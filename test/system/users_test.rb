a3c27a770f8fd570722c3de880c43b4b0b5db675
mistake-git <acbmstk0402@gmail.com>
2019-12-02 12:09:20 +0000

    テストを記述しました

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -13,7 +13,13 @@ class UsersTest < ApplicationSystemTestCase
     fill_in "users_emai", with: 'masa@example.com'
     fill_in "users_password",with:"<%= Devise::Encryptor.digest(User, 'password') %>"
     click_on "ログイン"
+    end
     
-  end
+    test "sign out" do
+    visit users_url
+    click_on "ログアウト"
+    end
+  
+  
     
 end
