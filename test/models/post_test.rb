7b10e2b0bac32c0309719c8905c0c8234628d77d
mistake-git <acbmstk0402@gmail.com>
2020-02-01 11:44:05 +0000

    投稿のモデルテストを作成しました

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -2,34 +2,35 @@ require 'test_helper'
 class PostTest < ActiveSupport::TestCase
     
    test "create_post" do
-     post = Post.create(name: 'タイ',number: 2,date: Date.new(2019,04,02),description:'hogehoge',feed:'ゴカイ',weather:'晴れ',address:'兵庫県')
-     assert_equal('タイ',post.name)
-     assert_equal(2,post.number)
-     assert_equal( Date.new(2019,04,02),post.date)
-     assert_equal('hogehoge',post.description)
-     assert_equal('ゴカイ',post.feed)
-     assert_equal('晴れ',post.weather)
-     assert_equal('兵庫県',post.address)
-   end
+        post = Post.create(name: 'タイ',number: 2,date: Date.new(2019,04,02),description:'hogehoge',feed:'ゴカイ',weather:'晴れ',address:'兵庫県',size: 30)
+        assert_equal('タイ',post.name)
+        assert_equal(2,post.number)
+        assert_equal( Date.new(2019,04,02),post.date)
+        assert_equal('hogehoge',post.description)
+        assert_equal('ゴカイ',post.feed)
+        assert_equal('晴れ',post.weather)
+        assert_equal('兵庫県',post.address)
+        assert_equal(30,post.size)
+    end
    
     test "update_post" do
-     post = posts :one
-     post.update!(name:'タイ',number: 2,date: Date.new(2019,04,02),description:'hogehoge',feed:'ゴカイ',weather:'晴れ',address:'兵庫県',
-     latitude:35.000001,longitude: 135.000001)
-     assert_equal('タイ',post.name)
-     assert_equal(2,post.number)
-     assert_equal(Date.new(2019,04,02),post.date)
-     assert_equal('hogehoge',post.description)
-     assert_equal('ゴカイ',post.feed)
-     assert_equal('晴れ',post.weather)
-     assert_equal('兵庫県',post.address)
+        post = posts :one
+        post.update!(name:'タイ',number: 2,date: Date.new(2019,04,02),description:'hogehoge',feed:'ゴカイ',weather:'晴れ',address:'兵庫県',
+        latitude:35.000001,longitude: 135.000001,size: 30)
+        assert_equal('タイ',post.name)
+        assert_equal(2,post.number)
+        assert_equal(Date.new(2019,04,02),post.date)
+        assert_equal('hogehoge',post.description)
+        assert_equal('ゴカイ',post.feed)
+        assert_equal('晴れ',post.weather)
+        assert_equal('兵庫県',post.address)
+        assert_equal(30,post.size)
    end
    
    test"destroy_post" do
-     post=Post.create(id:100,name: "fish",number: 2,date: Date.new(2019,04,02),description:'hogehoge',feed:'ゴカイ',weather:'晴れ',address:'兵庫県')
-     post.destroy
-     assert_empty(Post.where(id: 100))
-     
+        post=Post.create(id:100,name: "fish",number: 2,date: Date.new(2019,04,02),description:'hogehoge',feed:'ゴカイ',weather:'晴れ',address:'兵庫県')
+        post.destroy
+        assert_empty(Post.where(id: 100))
    end       
    
 end
