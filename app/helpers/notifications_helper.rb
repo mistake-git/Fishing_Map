8cfb364387a4b5d6ec41671234d1aec17f0aefe3
mistake-git <acbmstk0402@gmail.com>
2020-02-17 08:03:28 +0000

    ヘッダーの文法を統一しました

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -3,8 +3,7 @@ module NotificationsHelper
       @comment = nil
       visitor = link_to notification.visitor.name, "/users/#{notification.visitor.id}",style:"font-weight: bold; color: #00CCCC"
       your_post = link_to 'あなたの投稿',notification.post, style:"font-weight: bold; color: #00CCCC"
-      @post = Post.find_by(id:notification.post_id)
-      post_name = link_to "#{@post.name}",notification.post ,style:"font-weight: bold; color: #00CCCC"
+      fish_name = link_to "#{notification.post.name}",notification.post ,style:"font-weight: bold; color: #00CCCC"
       case notification.action
         when "follow" then
           "#{visitor}があなたをフォローしました"
@@ -14,7 +13,7 @@ module NotificationsHelper
           @comment=Comment.find_by(id:notification.comment_id).content
           "#{visitor}が#{your_post}にコメントしました"
         when "post" then
-        "#{visitor}が#{post_name}を釣り上げました"
+        "#{visitor}が#{fish_name}を釣り上げました"
       end
     end
     
