a705c4bc39b0cbf02215fcf88af71f231de5e348
mistake-git <acbmstk0402@gmail.com>
2020-03-30 01:14:36 +0000

    コミットを取り消します

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -4,7 +4,7 @@ class NotificationsController < ApplicationController
   def index
     @notifications = current_user.passive_notifications.all
     @notifications.where(checked: false).each do |notification|
-    notification.update_attributes(checked: true)
+      notification.update_attributes(checked: true)
     end
   end
   
