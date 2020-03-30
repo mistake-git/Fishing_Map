a111dac4eed35bda9a5db0b9554a9d68b8ff785d
mistake-git <acbmstk0402@gmail.com>
2020-03-30 01:20:30 +0000

    変更を修正しました

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -1,20 +1,20 @@
 module NotificationsHelper
     def notification_form(notification)
       @comment = nil
-      visitor = link_to notification.visitor.email, "/users/#{notification.visitor.id}",style:"font-weight: bold; color: #00CCCC"
+      visitor = link_to notification.visitor.name, "/users/#{notification.visitor.id}",style:"font-weight: bold; color: #00CCCC"
       case notification.action
         when "follow" then
             "#{visitor}があなたをフォローしました"
         when "like" then
-             your_post = link_to "あなたの#{notification.car.name}の投稿",notification.car, style:"font-weight: bold; color: #00CCCC"
+             your_post = link_to "あなたの#{notification.post.name}の投稿",notification.post, style:"font-weight: bold; color: #00CCCC"
             "#{visitor}が#{your_post}にいいね!しました"
         when "comment" then
-             your_post = link_to "あなたの#{notification.car.name}の投稿",notification.car, style:"font-weight: bold; color: #00CCCC"
+             your_post = link_to "あなたの#{notification.post.name}の投稿",notification.post, style:"font-weight: bold; color: #00CCCC"
             @comment = Comment.find_by(id:notification.comment_id).content
             "#{visitor}が#{your_post}にコメントしました"
-        when "car"then
-             car_name = link_to "#{notification.car.name}",notification.car ,style:"font-weight: bold; color: #00CCCC"
-            "#{visitor}さんの新しいプランを投稿しました。#{car_name}"
+        when "post" then
+             fish_name = link_to "#{notification.post.name}",notification.post ,style:"font-weight: bold; color: #00CCCC"
+            "#{visitor}が#{fish_name}を釣り上げました"
       end
     end
     
