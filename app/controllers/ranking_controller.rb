06e604a742e1dfdbba70bb7aa5ca2c22d33141d9
mistake-git <acbmstk0402@gmail.com>
2020-04-23 07:08:02 +0000

    地図ページのテキストを変更しました

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -2,7 +2,7 @@ class RankingController < ApplicationController
   
   def fish_size_ranking
       @post = Post.find_by(id: params[:id])
-      @posts = Post.where(name: @post.name).where("size IS NOT NULL").order(size: 'DESC').limit(8)
+      @posts = Post.where(name: @post.name).where("size IS NOT NULL").order(size: 'DESC').limit(8).page(params[:page]).per(8)
       @when_not_text = "サイズが登録された釣果がありません"
   end
   
