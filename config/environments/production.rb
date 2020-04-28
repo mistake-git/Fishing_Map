becf01b075e7241a9962407e70e704d7a4b5b622
mistake-git <acbmstk0402@gmail.com>
2020-04-28 14:18:36 +0000

    SSL化を一旦中止しました

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = {protocol: 'https', host: 'https://fishing-map.info'}
+  config.action_mailer.default_url_options = {protocol: 'http', host: 'http://fishing-map.info'}
   # mail setting
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
